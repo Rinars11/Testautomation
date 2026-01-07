2647a3870ee651f3a9e42d1c36f387f137f0f429
rinars11 <rina.rinkesh@gmail.com>
2026-01-06 23:32:09 -0500

    printing febonecci sequence using while loop

--- Basics/loop1.py
+++ Basics/loop1.py
@@ -15,9 +15,43 @@
 #         print("*", end = '')
 #     print("")
 
-x = int(input("Enter the number between 2 to 9: "))
-for i in range(1, x):
-    for j in range(1, i+1):
-        print("x", end = '')
-    print("")
-    
+# x = int(input("Enter the number between 2 to 9: 8"))
+# for i in range(1, x):
+#     for j in range(1, i+1):
+#         print("x", end = '')
+#     print("")
+
+# for i in range(20):
+#     if i%2 == 0:
+#         continue
+#     print(i)
+
+# for i in range(8):
+#     if i == 4:
+#         continue
+#     if i == 5:
+#         break
+#     print(i)
+
+# int: r 
+# r =8
+
+# for k in range (10,0,-2):
+#     if (k == 4):
+#         print (k)
+#     if ( k == r):
+#         print (r)
+
+j = int(input("Enter the number: "))
+x = 0
+y = 1
+z = x+y
+while(z <= j): 
+    print(z)
+    x = y
+    y = z
+    z = x + y
+
+
+
+
